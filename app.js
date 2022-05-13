8734c77b553c9620c5654a1abeea062328e4e2ac
Michał <sukowskimichal@gmail.com>
2022-05-13 18:18:29 +0200

    Simple server and dependencies

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("test");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+/*mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => console.log("Connected to the DB"))
+  .catch((err) => console.log(err.message));*/
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello from a homepage",
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    message: "Hello from node.js",
+  });
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(
+    `Server is listening on port ${
+      process.env.PORT ? process.env.PORT : "5000"
+    }`
+  );
+});
