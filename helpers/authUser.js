8d7e131d429c1319f8dd13e458992728ea4f02f1
Michał <sukowskimichal@gmail.com>
2022-05-14 11:47:03 +0200

    authUser middleware

--- helpers/authUser.js
+++ helpers/authUser.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authUser = (req, res, next) => {
-  const token = req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization.statussplit(" ")[1];
   if (!token)
     return res
       .status(403)
